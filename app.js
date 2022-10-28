046aae6051e38cee31378badc9e7d7b5d9f8e423
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-10-29 01:39:54 +0300

    feat(4): init mongoose

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 
+mongoose
+  .connect('mongodb+srv://admin:admin@cluster0.hmheqd3.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Conneted ok');
+  })
+  .catch((e) => {
+    console.log(e);
+    console.log('Connection error');
+  });
+  
 app.use(cors());
 app.use(express.json());
 app.use(routes);
